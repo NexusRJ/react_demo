4ff97af066986c5d3d6f4ac065d871d8746a0967
NexusRJ <nexusgrj@gmail.com>
2016-08-08 18:46:55 +0800

    Add articlelist component

--- app/static/js/components/article_list.js
+++ app/static/js/components/article_list.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react'
+import 'whatwg-fetch'
 
 class ArticleList extends Component {
+    constructor (props){
+        super(props);
+        this.state = {articles: []}
+    }
+    loadArticles (){
+        console.log('start download articles')
+        fetch('/api/articles').then(function(response){
+            return response.json()
+        }).then(function(json){
+            if (response.status == 200){
+                this.setState({articles: json['data']})
+            }
+        }).catch(function(ex){
+            console.log('parsing failed', ex)
+        })
+    }
     render () {
         return (
             <div>
-                Test ArticleList
+                <Article articles={this.state.articles} />
             </div>
         )
     }
